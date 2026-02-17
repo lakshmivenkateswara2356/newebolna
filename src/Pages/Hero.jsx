e263c70127742bcbabf13e4663b0242079e252f7
lakshmivenkateswara2356 <lakshmivenkateswarapunnam@gmail.com>
2026-02-17 08:49:05 +0530

    skdfsdj

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -121,7 +121,7 @@ const Hero = () => {
             </motion.div>
 
             <motion.div
-              
+              initial={{ opacity: 0, x: 60 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.8 }}
               viewport={{ once: true }}
